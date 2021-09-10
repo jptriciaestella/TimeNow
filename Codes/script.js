53d4b47e171d7c379ed1a5bd3e8b26975cd365ba
phascolarctostella <tricia.estella@binus.ac.id>
2021-09-10 18:51:53 +0700

    Navbar fix

--- Codes/script.js
+++ Codes/script.js
@@ -1,20 +1,31 @@
-window.onscroll = function() {scrollFunction()};
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var navbar = document.getElementById("navbar");
+  if (prevScrollpos > currentScrollPos) 
+  {
+    navbar.style.top = "0";
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+      navbar.classList.add("scrolled");
+    }
+    else
+    {
+      navbar.classList.remove("scrolled");
+    }
+  } 
+  else 
+  {
+    navbar.style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
-function scrollFunction() {
-   var navbar = document.getElementById("navbar");
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-     navbar.style.backgroundColor = "white";
-     navbar.classList.add("navbar-shadow");
-   } else {
-     navbar.style.backgroundColor = "rgba(255, 255, 255, 0)";
-     navbar.classList.remove("navbar-shadow");
-   }
- }
  
 function focusField(){
     document.getElementById("email-input").style.borderColor = "#1d1b67";
-  }
+}
 
 function blurField(){
     document.getElementById("email-input").style.borderColor = "#dadada";
-  }
+}
